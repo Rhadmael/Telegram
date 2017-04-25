183edfa5070e542d6ed7a2b0757bc7d6c7a21fa7
Rhadmael <nithushan.francis@gmail.com>
2017-04-25 07:42:37 +0530

    guide- initial server configs

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Telegram
 This project is made as a guide to be used for better understating of Node JS
+
+find the blog post at https://rhadmael.wordpress.com/2017/04/25/one-language-to-rule-them-all/
--- helloWorld.js
+++ helloWorld.js
@@ -1,10 +1,10 @@
 var http = require('http'),
 	host = '127.0.0.1',
-	port = '9000';
+	port = '9990';
 	
-var server = http.createServer(function(req , res){
-	res.writeHead(200, {'Content-Type' : 'text/html'});
-	res.end('<h1>Hello world ! </h1>');
+var server = http.createServer(function(request , response){
+	response.writeHead(200, {'Content-Type' : 'text/html'});
+	response.end('<h1>Hello World!</h1>');
 }).listen(port, host, function(){
-	console.log("server running on http:// " + host + " at " +  port);
+	console.log("server running on http://" + host + ":" +  port);
 })
